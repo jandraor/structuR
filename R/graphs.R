982db732506b31ec92c52ed2bc964b8433a4906a
Jair Andrade <jair.albert.andrade@gmail.com>
2020-03-16 00:31:19 +0000

    Adding graph for loop influence aat specific time

---
+++ R/graphs.R
@@ -0,0 +1,14 @@
+
+loop_influence_at_time <- function(time_point, leea_output) {
+  loop_analysis <- output_LEEA_num$loop_analysis %>%
+    filter(time == time_point)
+
+  ggplot2::ggplot(loop_analysis, aes(x = loop_influence_abs,
+                                    y = loop_influence_Re)) +
+    ggrepel::geom_text_repel(aes(label = loop_id), box.padding = 1) +
+    ggplot2::geom_point(size = 5, colour = "steelblue") +
+    ggplot2::facet_wrap(~eigenvalue_id) +
+    ggplot2::theme_bw() +
+    ggplot2::labs(x = "Abs[I]", y = "Re[I]",
+         title = paste0("Loop influence at time ", time_point))
+}
