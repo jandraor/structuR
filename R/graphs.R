d4571c049d70cd431df3f594e305b2a282c615b9
Jair Andrade <jair.albert.andrade@gmail.com>
2020-03-28 16:43:13 +0000

    Modifying loop influence graph

--- R/graphs.R
+++ R/graphs.R
@@ -5,11 +5,12 @@ loop_influence_at_time <- function(time_point, leea_output) {
 
   ggplot2::ggplot(loop_analysis, aes(x = loop_influence_abs,
                                     y = loop_influence_Re)) +
+    ggplot2::geom_hline(yintercept = 0, colour = "darkgrey") +
     ggrepel::geom_text_repel(aes(label = loop_id), box.padding = 1) +
     ggplot2::geom_point(size = 5, colour = "steelblue") +
     ggplot2::xlim(0, NA) +
     ggplot2::facet_wrap(~eigenvalue_id) +
-    ggplot2::theme_bw() +
+    ggplot2::theme_test() +
     ggplot2::labs(x = "Abs[I]", y = "Re[I]",
          title = paste0("Loop influence at time ", time_point))
 }
